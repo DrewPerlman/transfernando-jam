e59490854952f964b82e8811f0c143355cb0cc8f
DALTONS-LAPTOP\Dalton <dchancel@usc.edu>
2018-09-29 10:59:12 -0700

    menu screen with options and credits buttons

---
+++ Assets/Scripts/Menu Scripts/MenuButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour {
+	public GameObject optionsCanvas;
+	public GameObject creditsCanvas;
+	bool usable = true;
+	public GameObject[] menuButtons;
+	void Start(){
+		if(GameObject.Find(optionsCanvas.name)){
+			optionsCanvas.SetActive(false);
+		}
+		if(GameObject.Find(creditsCanvas.name)){
+			creditsCanvas.SetActive(false);
+		}
+	}
+	void Update(){
+		if(!GameObject.Find(optionsCanvas.name) && !GameObject.Find(creditsCanvas.name)){
+			usable = true;
+		}
+		for(int i=0;i<menuButtons.Length;i++){
+			menuButtons[i].GetComponent<Button>().interactable = usable;
+		}
+	}
+  public void StartGame()
+    {
+    	SceneManager.LoadScene(0);
+    }
+    public void ExitGame()
+    {
+    	Application.Quit();
+    }
+    public void Options()
+    {
+    	if(!GameObject.Find(optionsCanvas.name)){
+    		optionsCanvas.SetActive(true);
+    		usable = false;
+    	}
+    }
+    public void Credits()
+    {
+    	if(!GameObject.Find(creditsCanvas.name)){
+    		creditsCanvas.SetActive(true);
+    		usable = false;
+    	}
+    }
+}
