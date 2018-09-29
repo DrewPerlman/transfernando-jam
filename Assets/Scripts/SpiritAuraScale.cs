8378f7c37c726a1c4f8a642c630b0b3472304e1a
Gabriel Lacayo <glacay30@nca.edu.ni>
2018-09-29 13:18:57 -0700

    added aura masking, wall prefab, scaling of aura with arrow keys, and placeholder art

---
+++ Assets/Scripts/SpiritAuraScale.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritAuraScale : MonoBehaviour {
+
+    public float radius = 128f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            radius += 0.01f;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            radius -= 0.01f;
+        }
+
+
+        Vector3 auraScale = transform.localScale;
+        auraScale.x = radius * 2;
+        auraScale.y = radius * 2;
+        transform.localScale = auraScale;
+
+    }
+}
