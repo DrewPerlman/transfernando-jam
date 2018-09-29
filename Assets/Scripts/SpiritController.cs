2478ff83da7c82f111ef35dfeee5708cf4a2d7d5
nathanshangkala <shangkal@usc.edu>
2018-09-29 16:24:37 -0700

    flip wisp on switch side

--- Assets/Scripts/SpiritController.cs
+++ Assets/Scripts/SpiritController.cs
@@ -2,26 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SpiritController : MonoBehaviour {
 
+    public Transform cursor;
     public float moveSpeed = 0.05f;
-
+    private bool facingLeft = true;
+    Animator anim;
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
     
     // Update is called once per frame
     void Update () {
         FollowCursor();
+        float x = Input.mousePosition.x;
+        float halfOfScreen = Screen.width / 2;
+        if (x > halfOfScreen && facingLeft)
+            Flip();
+        else if (x < halfOfScreen && !facingLeft)
+            Flip();
     }
 
 
-    void FollowCursor() {
+    void FollowCursor()
+    {
         Vector3 mouseScreen = Input.mousePosition;
         Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(mouseScreen);
         mouseWorld.z = 0.0f;
-
         transform.position = Vector3.Lerp(transform.position, mouseWorld, moveSpeed);
+    }
+
+    void Flip()
+    {
+        facingLeft= !facingLeft;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
