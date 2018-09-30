37f9662f371341e870072ded4690bbc950c81d89
glacay30 <glacay30@nca.edu.ni>
2018-09-29 21:48:14 -0700

    added side door prefab, void door prefab, scene changing functionality with doors, and door assets

---
+++ Assets/Scripts/DoorSideBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorSideBehavior : MonoBehaviour {
+
+    private SpriteRenderer renderDoorClosed;
+    private SpriteRenderer renderDoorOpen;
+    private BoxCollider2D doorCollision;
+    
+    public bool doorTransition = false;
+    public bool doorOpen = false;
+
+    public void OpenDoor()
+    {
+            doorOpen = true;
+    }
+
+    private void Start()
+    {
+        renderDoorClosed = gameObject.GetComponentsInChildren<SpriteRenderer>()[0];
+        renderDoorOpen = gameObject.GetComponentsInChildren<SpriteRenderer>()[1];
+        doorCollision = gameObject.GetComponents<BoxCollider2D>()[1];
+
+        renderDoorClosed.enabled = true;
+        renderDoorOpen.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (doorOpen)
+        {
+            doorCollision.enabled = false;
+            renderDoorClosed.enabled = false;
+            renderDoorOpen.enabled = true;
+        }
+        else {
+            doorCollision.enabled = true;
+            renderDoorClosed.enabled = true;
+            renderDoorOpen.enabled = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && doorOpen && doorTransition && Input.GetKeyDown(KeyCode.W))
+        {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
---
+++ Assets/Scripts/DoorVoidBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorVoidBehavior : MonoBehaviour {
+
+    private SpriteRenderer renderDoorClosed;
+    private SpriteRenderer renderDoorOpen;
+    
+    public bool doorOpen = false;
+
+    public void OpenDoor()
+    {
+            doorOpen = true;
+    }
+
+    private void Start()
+    {
+        renderDoorClosed = gameObject.GetComponentsInChildren<SpriteRenderer>()[0];
+        renderDoorOpen = gameObject.GetComponentsInChildren<SpriteRenderer>()[1];
+
+        renderDoorClosed.enabled = true;
+        renderDoorOpen.enabled = false;
+    }
+
+    private void Update()
+    {
+        if (doorOpen)
+        {
+            renderDoorClosed.enabled = false;
+            renderDoorOpen.enabled = true;
+        }
+        else {
+            renderDoorClosed.enabled = true;
+            renderDoorOpen.enabled = false;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && doorOpen && Input.GetKeyDown(KeyCode.W))
+        {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
