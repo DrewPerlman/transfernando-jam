04abc929b658caaba305e7ce118549510c55eb3a
DALTONS-LAPTOP\Dalton <dchancel@usc.edu>
2018-09-30 15:57:55 -0700

    exit UI

---
+++ Assets/Scripts/GameQuitter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameQuitter : MonoBehaviour {
+
+	float maxTimer = 3f;
+	float timer = 0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!Input.GetKey(KeyCode.Escape)){
+				timer = 0f;
+			} else{
+				timer += Time.deltaTime;
+			}
+
+		if(timer == maxTimer){
+			Application.Quit();
+		}
+
+		GetComponent<Image>().fillAmount = timer/maxTimer;
+	}
+}
