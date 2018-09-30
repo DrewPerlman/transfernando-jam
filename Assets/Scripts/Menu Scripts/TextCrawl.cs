cee93c09bd1f7ad42bfb0a5137f47b9d4f58c07c
DALTONS-LAPTOP\Dalton <dchancel@usc.edu>
2018-09-29 17:52:59 -0700

    more title, opening text, and added background sprites

---
+++ Assets/Scripts/Menu Scripts/TextCrawl.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TextCrawl : MonoBehaviour {
+
+	public string textToWrite = "";
+	public Text textLocation;
+	string truText = "";
+	int index = 0;
+	float maxTimer = 0.05f;
+	float timer = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+		timer = maxTimer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timer > 0){
+			timer -= Time.deltaTime;
+		}
+
+		if(truText != textToWrite && timer <= 0f){
+			truText += textToWrite[index];
+			index += 1;
+			timer = maxTimer;
+		}
+
+		if(truText != textToWrite && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))){
+			truText = textToWrite;
+			timer = maxTimer;
+		}
+
+		if(index == textToWrite.Length){
+			truText = textToWrite;
+			timer = maxTimer;
+		}
+
+		textLocation.text = truText;
+
+		if(truText == textToWrite && timer <= 0f &&(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))){
+			SceneManager.LoadScene(0);
+		}
+	}
+}
